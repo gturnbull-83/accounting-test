7aa40365f33ce9a3ea573f4b2a27dadb335cc569
Gary Turnbull <garywturnbull@gmail.com>
2026-02-07 09:02:19 -0500

    Initial Commit

---
+++ Accounting test/Accounting_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accounting_testApp.swift
+//  Accounting test
+//
+//  Created by gary turnbull on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Accounting_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
